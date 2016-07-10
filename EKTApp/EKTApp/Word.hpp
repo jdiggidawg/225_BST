09a024119289f30ddfeffa2a356f4981740aef30
June Kim <jka79@asb9700-e03.csil.sfu.ca>
2016-07-10 14:00:43 -0700

    made Word files

---
+++ EKTApp/EKTApp/Word.hpp
@@ -0,0 +1,14 @@
+//
+//  Word.hpp
+//  EKTApp
+//
+//  Created by June Kim on 2016-07-10.
+//  Copyright © 2016 jdiggidawg. All rights reserved.
+//
+
+#ifndef Word_hpp
+#define Word_hpp
+
+#include <stdio.h>
+
+#endif /* Word_hpp */
