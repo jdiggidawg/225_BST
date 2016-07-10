1bd0aa80d93d446817134a1a129c577ccec103bd
Jessie Tran <pmtran@asb9700-e04.csil.sfu.ca>
2016-07-10 13:46:57 -0700

    a3 init

---
+++ EKTApp/EKTApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EKTApp
+//
+//  Created by Jessie Tran on 2016-07-10.
+//  Copyright © 2016 jdiggidawg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
